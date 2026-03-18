e18ef0af7ae3b5f20ea5f528818770cd40dc9156
SukaziN <nkosiyazisukazie@gmail.com>
2026-03-18 06:19:35 +0200

    added boolean argument to the event handler to reverse the direction of the bubbling events

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function bubble() {
 }
 let divs = document.getElementsByTagName("div");
 for (let i = 0; i < divs.length; i++) {
-  divs[i].addEventListener("click", bubble);
+  divs[i].addEventListener("click", bubble, true); //without the third argument "true" the events bubble from the inside out (5, 45, 345, 2345, 12345) while the true argument changes the bubbling direction and bubbles from the outside (12345, 2345, 345, 45, 5)
 }
